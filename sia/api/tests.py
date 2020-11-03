84084a13905ab98988a63009d5cc5f9ea63c78bd
misterk <keeper772@protonmail.com>
2020-11-03 18:57:00 +0200

    testy

--- sia/api/tests.py
+++ sia/api/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework.response import Response
 
-# Create your tests here.
+from .views import UploadViewSet
+
+class TestUploadViewSet(TestCase):
+    def test_list(self):
+
+        self.assertEqual(
+            UploadViewSet.list(self,TestUploadViewSet).data,
+            Response('GET API').data
+        )
+
+
