bd12d2aa48f9c2cd4b81c4233000e51d0a3fc8e2
kamil <kamilps1@gmail.com>
2020-11-03 15:42:55 +0100

    Added file endpoint

--- sia/api/views.py
+++ sia/api/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .serializers import UserSerializer, GroupSerializer, UploadSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class UploadViewSet(viewsets.ViewSet):
+    serializer_class = UploadSerializer
+
+    def list(self, request):
+        return Response("GET API")
+
+    def create(self, request):
+        file_uploaded = request.FILES.get('file_uploaded')
+        content_type = file_uploaded.content_type
+        response = "POST API and you have uploaded a {} file".format(file_uploaded.name)
+        return Response(response)
+
