c96fb25ee1e4b38621b5f5345ceb0ed6602ab7dd
Kuber19 <kubes2.all@gmail.com>
2020-11-29 11:45:35 +0100

    Requirements for FrontEnd team

--- sia/api/admin.py
+++ sia/api/admin.py
@@ -40,24 +40,3 @@ class ZgloszenieAdmin(admin.ModelAdmin):
 admin.site.register(Zgloszenia,ZgloszenieAdmin)
 
 
-class MiejsceAdmin(admin.ModelAdmin):
-    list_display = ('latitude', 'longitude',)
-
-    fieldsets = (
-        (None, {
-            'fields': ( 'latitude', 'longitude',)
-        }),
-    )
-
-    class Media:
-        if hasattr(settings, 'GOOGLE_MAPS_API_KEY') and settings.GOOGLE_MAPS_API_KEY:
-            css = {
-                'all': ('css/admin/location_picker.css',),
-            }
-            js = (
-                'https://maps.googleapis.com/maps/api/js?key={}'.format(settings.GOOGLE_MAPS_API_KEY),
-                'js/admin/location_picker.js',
-            )
-
-
-admin.site.register(Miejsce, MiejsceAdmin)
--- sia/api/models.py
+++ sia/api/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class OcenyZgloszen(models.Model):
     ocena_zgloszenia_id = models.AutoField(primary_key=True)
-    zgloszenie = models.ForeignKey('Zgloszenia', models.DO_NOTHING)
-    user = models.ForeignKey('Uzytkownik', models.DO_NOTHING)
-    ocena = models.IntegerField()
-    komentarz = models.TextField()
+    zgloszenie          = models.ForeignKey('Zgloszenia', models.DO_NOTHING)
+    user                = models.ForeignKey('Uzytkownik', models.DO_NOTHING)
+    ocena               = models.IntegerField(
+                            default=None,
+                            validators=[MaxValueValidator(5), MinValueValidator(1)])
 
     class Meta:
         managed = True
@@ -18,11 +19,11 @@ class OcenyZgloszen(models.Model):
 
 class RodzajZgloszenia(models.Model):
     rodzaj_zgloszenia_id = models.AutoField(primary_key=True)
-    kategoria = models.TextField()
-    priorytet = models.IntegerField()
+    kategoria            = models.TextField()
+    priorytet            = models.IntegerField()
 
     class Meta:
-        managed = True
+        managed  = True
         db_table = 'RodzajZgloszenia'
     
     def __str__(self):
@@ -30,15 +31,15 @@ class RodzajZgloszenia(models.Model):
 
 
 class Uzytkownik(models.Model):
-    user_id = models.AutoField(primary_key=True)
-    nick = models.TextField()
-    imie = models.TextField()
-    nazwisko = models.TextField()
-    organizacja = models.TextField()
-    uprawnienia = models.IntegerField()
+    user_id         = models.AutoField(primary_key=True)
+    nick            = models.TextField()
+    imie            = models.TextField()
+    nazwisko        = models.TextField()
+    email           = models.TextField()
+    organizacja     = models.TextField()
+    uprawnienia     = models.IntegerField()
     liczba_zgloszen = models.IntegerField()
-    haslo = models.TextField()
-    awatar = models.TextField(blank=True, null=True)
+    haslo           = models.TextField()
 
     class Meta:
         managed = True
@@ -47,36 +48,28 @@ class Uzytkownik(models.Model):
     def __str__(self):
         return f"{self.nick}"
 
-class Miejsce(models.Model):
-    latitude = models.DecimalField(
-                max_digits=9, decimal_places=6, null=True, blank=True)
-    longitude = models.DecimalField(
-                max_digits=9, decimal_places=6, null=True, blank=True)
-
 
 class Zgloszenia(models.Model):
-    zgloszenie_id = models.AutoField(primary_key=True)
-    tytul_zgloszenia = models.TextField(null=True)
-    user = models.ForeignKey(
-        'Uzytkownik', models.DO_NOTHING, blank=True, null=True
-    )
-    rodzaj_zgloszenia = models.ForeignKey(
-        'RodzajZgloszenia', models.DO_NOTHING, blank=True, null=True
-    )
-    sciezka_do_pliku = models.TextField(blank=True, null=True)
-    latitude = models.DecimalField(
-                max_digits=9, decimal_places=6, null=True, blank=True)
-    longitude = models.DecimalField(
-                max_digits=9, decimal_places=6, null=True, blank=True)
-    data_czas = models.DateTimeField()
-    akceptacja = models.IntegerField()
-    opis = models.TextField()
+    zgloszenie_id       = models.AutoField(primary_key=True)
+    tytul_zgloszenia    = models.TextField(null=True)
+    user                = models.ForeignKey(
+                        'Uzytkownik', models.DO_NOTHING, blank=True, null=True)
+    rodzaj_zgloszenia   = models.ForeignKey(
+                        'RodzajZgloszenia', models.DO_NOTHING, blank=True, null=True)
+    sciezka_do_pliku    = models.TextField(blank=True, null=True)
+    latitude            = models.DecimalField(
+                          max_digits=9, decimal_places=6, null=True, blank=True)
+    longitude           = models.DecimalField(
+                          max_digits=9, decimal_places=6, null=True, blank=True)
+    data_czas           = models.DateTimeField()
+    akceptacja          = models.IntegerField()
+    opis                = models.TextField()
 
     def __str__(self):
         return "Zgloszenie nr {}, Opis : {}".format(str(self.zgloszenie_id),str(self.opis))
 
     class Meta:
-        managed = True
+        managed  = True
         db_table = 'Zgloszenia'
 
 
